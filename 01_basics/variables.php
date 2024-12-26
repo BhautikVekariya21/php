d237b00f3441a874ee2b1b3b2a95f5a71d8b9984
BhautikVekariya21 <bhautikvekariya1123@gmail.com>
2024-12-26 19:20:44 +0530

    add variables.php script to demonstrate PHP variable usage and echo

---
+++ 01_basics/variables.php
@@ -0,0 +1,13 @@
+<?php
+// Variables store data and can be used later in the program.
+// PHP variables start with a $ symbol and can store different data types.
+
+$name = "John"; // String variable
+$age = 25; // Integer variable
+$isStudent = true; // Boolean variable
+
+// Displaying variables using echo
+echo "Name: $name<br>";
+echo "Age: $age<br>";
+echo "Is Student: " . ($isStudent ? "Yes" : "No");
+?>
