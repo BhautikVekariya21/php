1ca9eef64eac4e4f7a48a2ad3ab0dee9025c7465
BhautikVekariya21 <bhautikvekariya1123@gmail.com>
2024-12-26 21:49:18 +0530

    add data_types.php script to demonstrate PHP data types and their usage

---
+++ 01_basics/data_types.php
@@ -0,0 +1,21 @@
+<?php
+// PHP supports various data types: strings, integers, floats, booleans, arrays, and objects.
+
+// String
+$string = "Hello, PHP!";
+
+// Integer
+$integer = 42;
+
+// Float
+$float = 3.14;
+
+// Boolean
+$boolean = true;
+
+// Display data types using gettype()
+echo "Data Type of \$string: " . gettype($string) . "<br>";
+echo "Data Type of \$integer: " . gettype($integer) . "<br>";
+echo "Data Type of \$float: " . gettype($float) . "<br>";
+echo "Data Type of \$boolean: " . gettype($boolean) . "<br>";
+?>
